1fca21915d725cff249dddca6be43ae1fecbfa4e
Tamilly costa <Tamillycosta899@gmail.com>
2025-01-14 21:40:44 -0300

    ✨ feat: implementa carregamento dinamico das paginas do painel

--- classes/Painel.php
+++ classes/Painel.php
@@ -10,6 +10,18 @@ class Painel{
         header(  'Location: '.INCLUDE_PAINEL_PATH);
         
     }
+    public static function carregarPagina():void{
+        if(isset($_GET['url'])){
+           $url = explode('/',$_GET['url']);
+           if(file_exists('pages/'.$url[0].'.php')){
+                include('pages/'.$url[0].'.php');
+           }else{
+                 header(  'Location: '.INCLUDE_PAINEL_PATH);
+           }
+        }else{
+            include('pages/home.php');
+        }
+    }
 }
 
 ?>
