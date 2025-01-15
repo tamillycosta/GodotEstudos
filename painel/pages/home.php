3f92e3638d0f5b3886d3a227fffebf3168d6156f
Tamilly costa <Tamillycosta899@gmail.com>
2025-01-14 21:42:26 -0300

    ✨ feat: incia as pages do painel

---
+++ painel/pages/home.php
@@ -0,0 +1,23 @@
+<section class="main-container">
+    <section class="dashBoard">
+
+    <div class="dashboard-elements">
+        <h2>10 Usuarios Online!</h2>
+        <i class="fa-solid fa-users"></i>
+        
+    </div>
+
+    <div class="dashboard-elements">
+        <h2>10 Total de Visitas!</h2>
+        <i class="fa-solid fa-envelope"></i>
+        
+    </div>
+
+    <div class="dashboard-elements">
+        <h2>10 Visitas Hoje!</h2>
+        <i class="fa-solid fa-phone"></i>
+    
+    </div>
+
+    </section>
+</section>
