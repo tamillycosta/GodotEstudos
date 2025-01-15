04cef9fad9aeafcb1588dff22aad0bd08dc26607
Tamilly costa <Tamillycosta899@gmail.com>
2025-01-15 15:35:19 -0300

    feat ✨: implementa tabela de apresentação de usuários online

--- painel/pages/home.php
+++ painel/pages/home.php
@@ -1,8 +1,15 @@
 <section class="main-container">
-    <section class="dashBoard">
 
+    <section class="dashBoard w_100 box_shadow">
+
+    <?php
+        $Painel = new Painel();
+        $numUsuarios = $Painel->listarUsuariosOnline();
+    ?>
+    
     <div class="dashboard-elements">
-        <h2>10 Usuarios Online!</h2>
+    <h2><?php echo count($numUsuarios)?>
+        Usuarios Online!</h2>
         <i class="fa-solid fa-users"></i>
         
     </div>
@@ -18,6 +25,42 @@
         <i class="fa-solid fa-phone"></i>
     
     </div>
+
+    </section>
+
+    <section class="user-table w_100 box_shadow">
+        <div class="box-title">
+        <i class="fa-solid fa-users"></i>
+        <h2>Usuários Online</h2>
+        </div>
+
+    
+        <div class="row">
+            <div class="col">
+                <span>IP</span>
+            </div><!--col-->
+            <div class="col">
+                <span>Ultima Ação</span>
+            </div><!--col-->
+        </div>
+
+        <?php
+        for($i = 0; $i<=10; $i++ )
+        {
+        ?>
+
+        <div class="row">
+            <div class="col">
+                <span>190.190.190</span>
+            </div><!--col-->
+            <div class="col">
+                <span>19/09/2017 19:00:00</span>
+            </div><!--col-->
+            <div class="clear" ></div>
+        </div>
+            <?php
+        }
+            ?>
 
     </section>
 </section>
