42129e803c1899afd95988f22fb3c9ebf61fe38d
Tamilly costa <Tamillycosta899@gmail.com>
2025-01-28 14:45:50 -0300

    refactor ♻️:: modifica autoloud de classes para o composer.json

--- config.php
+++ config.php
@@ -3,25 +3,12 @@ session_start();
 date_default_timezone_set('America/Sao_Paulo');
 require __DIR__ . '/vendor/autoload.php'; // Caminho relativo ao autoloader do Composer
 
-$autoloud = function($class): void {
-    $file = 'classes/' . $class . '.php';
-    $file2 = '../classes/'. $class . '.php';
-    
-    if (file_exists($file)) {
-        include $file;
-    }
-    else if(file_exists($file2)) {
-        include $file2;
-    } else {
-        echo "Erro: O arquivo da classe {$class} não foi encontrado!";
-    }
-};
 
-// Registra a função de autoload
-spl_autoload_register($autoloud);
-
-define('INCLUDE_PATH', 'http://localhost/Web-Page-PHP-/');
+define('INCLUDE_PATH', 'http://localhost/Web-Page-PHP/');
 define('INCLUDE_PAINEL_PATH', INCLUDE_PATH.'painel/');
+define('BASE_DIR_PAINEL', __DIR__.'/painel');
+    
+
 
 
 
